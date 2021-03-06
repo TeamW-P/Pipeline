6e8a2bab966c453a89572ae4ca8fc6ebfeeba62d
Rayan Osseiran <rayano24@Hotmail.com>
2021-03-06 12:56:45 -0500

    set-up controller & service for rnamigos
    - adds controller/call for pipeline endpoint

--- Pipeline/controllers/RnaMigosController.py
+++ Pipeline/controllers/RnaMigosController.py
@@ -2,8 +2,18 @@
 # basically whatever data manipulation, or combination of service calls we need to do will happen here
 # the routing functions will basically call the controller functions to do what needs to be done
 from services.RnaMigos import *
+import tempfile
+
 class RnaMigos:
 
     @staticmethod
-    def greeting():
-        return greeting()
+    def rnamigos_pipeline(representative_graphs, ligand_library):
+        ligand_Library.seek(0)
+        # store the file temporarily
+        temp = tempfile.NamedTemporaryFile(
+            suffix="." + ligand_Library.filename.rsplit(".", 1)[1])
+        temp.write(ligand_Library.read())
+        temp.seek(0)
+        result = rnamigos_pipeline(representative_graphs, temp.name)
+        temp.close()
+        return result
--- Pipeline/services/RnaMigos.py
+++ Pipeline/services/RnaMigos.py
@@ -2,9 +2,18 @@
 # it'll only be pure http call, as in send the inputs and receive the outputs
 # manipulation of that data should be done elsewhere
 import requests 
-URL = "http://rnamigos:5002/" 
+URL = "http://0.0.0.0:5002/" 
 
-def greeting():
-    r = requests.get(url = URL) 
-    data = r.json() 
-    return data
+def rnamigos(representative_graphs, ligand_Library):
+    '''
+    Makes a call to the BayesPairing endpoint to retrieve representative graphs
+
+    :param payload: the payload to send
+    :return: the raw response from BayesPairing
+    '''
+    files = [("library", open(ligand_Library, "rb"))]
+    data = {"graphs": str(representative_graphs)}
+    headers = {}
+    response = requests.request(
+        "POST", URL + "rnamigos_pipeline", headers=headers, data=data, files=files)
+    return response
